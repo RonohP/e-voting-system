f8b339c8318546b8d3eb7be1969e08d8907c18ec
RonohP <ronopurity05@gmail.com>
2021-08-26 21:10:15 +0300

    add header section

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Fragment } from 'react';
-import NavBar from '../components/NavBar/NavBar';
 import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
 
 function Landing() {
     return (
         <Fragment>
-            <NavBar/>
+            <Header/>
             <Footer/>
         </Fragment>
     );
