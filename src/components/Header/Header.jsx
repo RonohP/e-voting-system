f3c57780691bd6bf1982f7d44af4be168dda8c88
RonohP <ronopurity05@gmail.com>
2021-08-27 23:19:45 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,19 +27,6 @@ function Header() {
             />
           </div>
         </div>
-        {/* <div className='hero-text absolute pt-20 w-3/5 left-24 top-80'>
-          <h1 className='font-bold text-5xl'>
-            Fast, Secured and Accessible Voting System
-          </h1>
-          <br />
-          <p>
-            Let’s make voting and elections easy for you. This is designed to
-            ensure a secured voting session.
-          </p>
-        </div>
-        <div className='hero-img h-auto absolute left-96 top-80 w-96 h-96 object-contain'>
-          <img src={header} alt='header' className='' />
-        </div> */}
       </header>
     );
 }
