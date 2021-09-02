049c161a193f97697d9527c865474348d7e27151
Purity Rono <38761529+RonohP@users.noreply.github.com>
2021-09-02 19:32:45 +0300

    Revert "Navbar"

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -29,7 +29,7 @@ function NavBar() {
           </svg>
         </div>
         <div className='pr-6 pt-8 md:block hidden'>
-          <Link className='p-4' to='/home'>
+          <Link className='p-4' to='/'>
             Home
           </Link>
           <Link className='p-4' to='/about'>
