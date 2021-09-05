9b5ddcc9a596197e2662e1e24b1eeeb77b77face
Bilikis Sabitu <sabitubilikis96@gmail.com>
2021-09-05 16:33:24 +0100

    pushing the login and signup page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import FAQ from './pages/FAQ';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 import PageNotFound from './pages/PageNotFound';
 import ThankYou from './pages/ThankYou';
 
@@ -20,6 +22,8 @@ function App() {
           <Route exact path='/contact' component={Contact} />
           <Route exact path='/thanks' component={ThankYou} />
           <Route exact path='/faq' component={FAQ} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
           <Route exact path='*' component={PageNotFound} />
         </Switch>
       </Router>
