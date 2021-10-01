00d90e5a45f604c651d75ab4e7bf95ea9200327c
RonohP <ronopurity05@gmail.com>
2021-10-01 17:52:54 +0300

    BarChart added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,19 +9,19 @@ function Header() {
     <header className="header mt-4 relative pl-8 pr-8 flex flex-col h-screen w-full">
       <NavBar />
       <div className="hero mt-2 flex flex-row h-screen w-full pt-10">
-        <div className="hero-text flex flex-col pt-36 pl-10 w-1/2">
-          <h1 className="font-bold text-5xl ml-24">
+        <div className="hero-text flex flex-col  w-1/2">
+          <h1 className="font-bold text-5xl ml-24 mt-56">
             Fast, Secured and Accessible Voting System
           </h1>
           <br />
-          <p className="mb-6 ml-24">
+          <p className="mb-6 ml-24 text-xl">
             {" "}
             Let’s make voting and elections easy for you. This is designed to
             ensure a secured voting session.
           </p>
           <br />
           <button className="register-btn border rounded-full py-2 px-8 border-solid w-60 ml-24">
-            <Link className="p-4" to="/register">
+            <Link className="p-4 font-bold" to="/register">
               Register as a Voter
             </Link>
           </button>
